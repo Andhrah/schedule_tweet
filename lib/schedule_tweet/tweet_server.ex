a175134c70e86b89af2a6970002ed495b145eee3
Alexandra Collins <andracolexa@gmail.com>
2020-03-15 13:07:36 +0100

    add comment to tweet server file

--- lib/schedule_tweet/tweet_server.ex
+++ lib/schedule_tweet/tweet_server.ex
@@ -1,7 +1,14 @@
 defmodule ScheduleTweet.TweetServer do
+  # GenServer(generic server) is a way to send messages to a process. 
+  # Its also a way to make sure that even if the process dies that
+  # we'll be able to restart it.
   use GenServer
 
+  # start_link() function only calls Genserver start_link function
   def start_link(_) do
+    # Genserver.start_link takes a first parameter
+    # of dunder module which will reference current module.
+    # While the second parameter is the initial state of the server.
     GenServer.start_link(__MODULE__, :ok, name: :tweet_server)
   end
 
@@ -9,6 +16,7 @@ defmodule ScheduleTweet.TweetServer do
     {:ok, %{}}
   end
 
+  # This is an asynchronous function that sends out tweet
   def handle_cast({:tweet, tweet}, _) do
     ScheduleTweet.Tweet.send(tweet)
     {:noreply, %{}}
