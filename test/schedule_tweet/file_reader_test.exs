b7aa167227f7a05d5fea990e3c697813390964f3
Alexandra Collins <andracolexa@gmail.com>
2020-03-15 15:22:39 +0100

    write unit test

---
+++ test/schedule_tweet/file_reader_test.exs
@@ -0,0 +1,21 @@
+defmodule FileReaderTest do
+  use ExUnit.Case
+  import ScheduleTweet.FileReader
+  test "passing a file should return a string" do
+    str = get_strings_to_tweet(Path.join("#{:code.priv_dir(:schedule_tweet)}", "sample.txt"))
+
+    assert str != nil
+  end
+
+  test "Will not return a string longer than 140 character" do
+    str = get_strings_to_tweet(Path.join("#{:code.priv_dir(:schedule_tweet)}", "/test/too_long.txt"))
+
+    assert str == "Short line."
+  end
+
+  test "An empty string should return an empty string" do
+    str = pick_string("")
+
+    assert str == ""
+  end
+end
